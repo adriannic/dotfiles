731ca25dd112508d1fa9a872f7bc7b18853c61b8
adriannic <spamadriannic@gmail.com>
2023-10-23 15:43:34 +0200

    Swap neofetch with paleofetch (same but faster)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    [ "$USER" = "adriannic" ] && neofetch
+    [ "$USER" = "adriannic" ] && paleofetch
 
     # Aliases
     alias ls="lsd"
