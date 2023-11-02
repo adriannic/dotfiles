cf293c764f6752ecddb399b2fee7f46ff7bc67a0
adriannic <spamadriannic@gmail.com>
2023-11-02 17:32:44 +0100

    Fish: Convert to gif at 15fps

--- .config/fish/functions/togif.fish
+++ .config/fish/functions/togif.fish
@@ -1,4 +1,4 @@
 function togif --wraps='ffmpeg -i $argv[1] -vf "split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=new=1:dither=none" -loop -1 $argv[2]' --description 'Convert video to gif without dithering'
-  ffmpeg -i $argv[1] -vf "split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=new=1:dither=none" -loop -1 $argv[2]
+  ffmpeg -i $argv[1] -vf "split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=new=1:dither=none" -loop -1 -r 15 $argv[2]
         
 end
