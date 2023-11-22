a0a30ded49379aee7dca1d46a00e497c6da8f40d
adriannic <spamadriannic@gmail.com>
2023-11-22 01:31:31 +0100

    Ags: Change Workspaces widget

--- .config/ags/settings.js
+++ .config/ags/settings.js
@@ -1,3 +1,14 @@
 export default {
   ANIMATION_SPEED_IN_MILLIS: 200,
+  workspaceList: [
+    { name: "Principal", index: 1 },
+    { name: "Secundario", index: 2 },
+    { name: "Juegos", index: 3 },
+    { name: "Navegador", index: 4 },
+    { name: "Telegram", index: 5 },
+    { name: "Discord", index: 6 },
+    { name: "Whatsapp", index: 7 },
+    { name: "Spotify", index: 8 },
+    { name: "Obs", index: 9 },
+  ],
 };
