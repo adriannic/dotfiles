844acce0245be637b2052a14061c7ec33b645e91
adriannic <spamadriannic@gmail.com>
2024-01-17 17:28:37 +0100

    Wal: Notify when changing the background image

--- .config/wal/wp.sh
+++ .config/wal/wp.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
 function wallpaper() {
+	tmp="$(mktemp).png"
+	ffmpeg -i "$1" -vf "select=eq(n\,0)" -q:v 3 "$tmp"
 	(
-		swww img "$@" -t none &
-		wal -n -i "$@"
+		swww img "$1" -t none &
+		wal -n -i "$1"
 		pywalfox update &
 	  bash ~/.config/hypr/scripts/pywal.sh &
 	  swaync-client -rs &
+	  notify-send -i "$tmp" "Fondo de pantalla cambiado" "Fondo de pantalla y esquema de colores cambiado a $(basename "$1")"
 	  killall ags && ags &
 	  disown
 	) > /dev/null 2>&1
