5768fb506de1e638af9e97f3810d71c8e68c144b
adriannic <spamadriannic@gmail.com>
2024-01-17 17:36:21 +0100

    Wal: Simplify random-bg script

--- .config/wal/random-bg.sh
+++ .config/wal/random-bg.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 
-i=0
-for bg in ~/Imágenes/Fondos/*; do
-  array[i]=$bg;
-  ((i=i+1))
-done
+bgs=(~/Imágenes/Fondos/*)
 
-size=${#array[@]}
+size=${#bgs[@]}
 index=$((RANDOM % size))
 
-~/.config/wal/wp.sh "${array[$index]}"
+~/.config/wal/wp.sh "${bgs[$index]}"
