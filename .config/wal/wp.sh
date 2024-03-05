99c645b34a0e357af4191f0fb29e5aa37d7a392b
adriannic <spamadriannic@gmail.com>
2024-03-05 19:34:05 +0100

    RandomBG: Fix bg change on NixOS

--- .config/wal/wp.sh
+++ .config/wal/wp.sh
@@ -7,12 +7,12 @@ function wallpaper() {
 		swww img "$1" -t none &
 		wal -n -i "$1"
 		pywalfox update &
-	  bash ~/.config/hypr/scripts/pywal.sh &
-	  swaync-client -rs &
-	  notify-send -i "$tmp" "Fondo de pantalla cambiado" "Fondo de pantalla y esquema de colores cambiado a $(basename "$1")"
-	  killall ags && ags &
-	  disown
-	) > /dev/null 2>&1
+		bash ~/.config/hypr/scripts/pywal.sh &
+		swaync-client -rs &
+		notify-send -i "$tmp" "Fondo de pantalla cambiado" "Fondo de pantalla y esquema de colores cambiado a $(basename "$1")"
+		pkill ags && ags &
+		disown
+	) >/dev/null 2>&1
 }
 
 wallpaper "$1"
