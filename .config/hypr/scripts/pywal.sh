ce1e3cf98af7b653ce28c0f3a6f9362d276ee593
adriannic <nicolas.aguilera.adrian@gmail.com>
2024-11-20 17:51:29 +0100

    feat(hypr): tweak animations

--- .config/hypr/scripts/pywal.sh
+++ .config/hypr/scripts/pywal.sh
@@ -5,4 +5,4 @@ source "$HOME/.cache/wal/colors.sh"
 background=${background:1}
 color2=${color2:1}
 
-hyprctl keyword general:col.active_border "rgb($background) rgb($color2) 0deg"
+hyprctl keyword general:col.active_border "rgb($color2)"
