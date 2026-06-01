5561a230ff61933f95d0bafded8d0cba1efb852f
adriannic <nicolas.aguilera.adrian@gmail.com>
2026-06-01 18:25:18 +0200

    fix(hypr): fix login script

--- .config/hypr/login.sh
+++ .config/hypr/login.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-for i in $(hyprctl monitors -j | jq '.[].id'); do
+MONITORS=$(hyprctl monitors -j | jq '.[].id')
+for i in $MONITORS; do
   hyprctl dispatch "hl.dsp.exec_cmd(\"mpv --fs --pause --no-input-default-bindings --no-input-cursor --input-ipc-server=/tmp/mpvsocket-$i ~/.config/hypr/login.mkv\", { monitor = \"$i\" , no_anim = true, stay_focused = true })"
 done
 
-for socket in /tmp/mpvsocket-*; do
-  (sleep 5 && echo 'cycle pause' | socat - "$socket") &
+for i in $MONITORS; do
+  (sleep 5 && echo 'cycle pause' | socat - "/tmp/mpvsocket-$i") &
 done
